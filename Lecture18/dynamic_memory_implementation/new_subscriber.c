42e0340542e09ecacf2b2be49f73a6a39b11b067
Bytbager <sdgrfdghgfg@gmail.com>
2023-09-21 14:55:43 +0700

    test

--- Lecture18/dynamic_memory_implementation/new_subscriber.c
+++ Lecture18/dynamic_memory_implementation/new_subscriber.c
@@ -10,32 +10,14 @@ int new_subscriber(int *subscriber_count, struct People *list) {
         quit_program(subscriber_count, list, err);
     }
     printf("Input subscriber's name:\n");
-    scanf("%s", list[*subscriber_count].name);
-    list[*subscriber_count].name = malloc(strlen(buffer) + 1);
-    if (list[*subscriber_count].name == NULL) {
-        perror("List.name didn't get memory!\n");
-        err = ERROR;
-        quit_program(subscriber_count, list, err);
-    }
-    strncat(list[*subscriber_count].name, buffer, strlen(buffer) + 1);
+    scanf("%s", buffer);
+    list[*subscriber_count].name = strndup(buffer, strlen(buffer));
     printf("Input subscriber's surname:\n");
-    scanf("%s", list[*subscriber_count].surname);
-    list[*subscriber_count].surname = malloc(strlen(buffer) + 1);
-    if (list[*subscriber_count].surname == NULL) {
-        perror("List.surname didn't get memory!\n");
-        err = ERROR;
-        quit_program(subscriber_count, list, err);
-    }
-    strncat(list[*subscriber_count].surname, buffer, strlen(buffer) + 1);
+    scanf("%s", buffer);
+    list[*subscriber_count].surname = strndup(buffer, strlen(buffer));
     printf("Input subscriber's phone number:\n");
-    scanf("%s", list[*subscriber_count].phone_number);
-    list[*subscriber_count].phone_number = malloc(strlen(buffer) + 1);
-    if (list[0].phone_number == NULL) {
-        perror("List.phone_number didn't get memory!\n");
-        err = ERROR;
-        quit_program(subscriber_count, list, err);
-    }
-    strncat(list[*subscriber_count].phone_number, buffer, strlen(buffer) + 1);
+    scanf("%s", buffer);
+    list[*subscriber_count].phone_number = strndup(buffer, strlen(buffer));
     printf("New subscriber added!\n");
     *subscriber_count+=1;
     free(buffer);
