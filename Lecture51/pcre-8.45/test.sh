c983032b83345daa709209a9f2c48f96563f4693
Bytbager <sdgrfdghgfg@gmail.com>
2024-03-13 01:43:46 +0700

    mc build

--- Lecture51/pcre-8.45/test.sh
+++
@@ -1,6 +0,0 @@
-LINARO=/usr/gcc-linaro-11.3.1-2022.06-x86_64_arm-linux-gnueabihf/bin
-PATH=$LINARO:$PATH
-./configure \
---prefix=$PWD/_install \
---host=arm-linux-gnueabihf
-# --with-z=/home/bytbager/My_projects/Eltex/Lecture51/zlib-1.3.1/_install \
