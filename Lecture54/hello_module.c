f9f111969b586fc751a7c27f1c2d159e32d1d062
Bytbager <sdgrfdghgfg@gmail.com>
2024-03-14 17:08:34 +0700

    proc added

--- Lecture54/hello_module.c
+++ Lecture54/hello_module.c
@@ -5,11 +5,17 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/proc_fs.h>
 #include <linux/rwlock.h>
 #include <linux/version.h>
 
 #define DEVICE_NAME "hellodev"
+#define PROC_NAME "helloproc"
 #define STRING_LEN 64
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+#define HAVE_PROC_OPS
+#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Bytbager");
@@ -19,13 +25,12 @@ static int major;
 static rwlock_t lock;
 static struct class *cls;
 static char hello_string[STRING_LEN] = "Hello!\n\0";
+static struct proc_dir_entry *proc_file;
 
 enum {
     DEV_NOT_USED = 0,
     DEV_OPENED = 1,
 };
-
-static atomic_t already_open = ATOMIC_INIT(DEV_NOT_USED);
 
 ssize_t hello_dev_read(struct file *fd, char __user *buff, size_t size, loff_t *off) {
     size_t rc;
@@ -49,20 +54,54 @@ ssize_t hello_dev_write(struct file *fd, const char __user *buff, size_t size, l
     return rc;
 }
 
+ssize_t hello_proc_read(struct file *fd, char __user *buff, size_t size, loff_t *off) {
+    size_t rc;
+
+    read_lock(&lock);
+    rc = simple_read_from_buffer(buff, size, off, hello_string, STRING_LEN);
+    read_unlock(&lock);
+
+    return rc;
+}
+
+ssize_t hello_proc_write(struct file *fd, const char __user *buff, size_t size, loff_t *off) {
+    size_t rc;
+
+    if (size > STRING_LEN) return -EINVAL;
+
+    write_lock(&lock);
+    rc = simple_write_to_buffer(hello_string, STRING_LEN, off, buff, size);
+    write_unlock(&lock);
+
+    return rc;
+}
+
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .read = hello_dev_read,
     .write = hello_dev_write
 };
 
+#ifdef HAVE_PROC_OPS
+static struct proc_ops pops = {
+    .proc_read = hello_proc_read,
+    .proc_write = hello_proc_write
+};
+#else
+static struct file_operations pops = {
+    .read = hello_proc_read,
+    .write = hello_proc_write
+};
+#endif
+
 static int __init hello_module_init(void) {
     pr_info("Hello, World!\n");
     
+    rwlock_init(&lock);
+
     major = register_chrdev(0, DEVICE_NAME, &fops);
     if (major < 0) return major;
     pr_info("I was assigned a new major number: %d\n", major);
-    
-    rwlock_init(&lock);
     
     #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
         cls = class_create(DEVICE_NAME);
@@ -70,8 +109,15 @@ static int __init hello_module_init(void) {
         cls = class_create(THIS_MODULE, DEVICE_NAME);
     #endif
     device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-    
     pr_info("Device created: /dev/%s\n", DEVICE_NAME);
+
+    proc_file = proc_create(PROC_NAME, 0666, NULL, &pops);
+    if (proc_file == NULL) {
+        proc_remove(proc_file);
+        pr_info("proc_create error!\n");
+        return -ENOMEM;
+    }
+    pr_info("Proc created: /proc/%s\n", PROC_NAME);
 
     return 0;
 }
@@ -81,6 +127,8 @@ static void __exit hello_module_exit(void) {
     class_destroy(cls);
     unregister_chrdev(major, DEVICE_NAME);
     
+    proc_remove(proc_file);
+
     pr_info("Goodbye, World!\n");
 }
 
